65cf13a51c5164d8e605e4249ac6db42700162d5
felix huari <felixhuari@gmail.com>
2024-08-15 11:38:25 -0500

    Cambio 1 configuraciones

--- src/App.vue
+++ src/App.vue
@@ -4,12 +4,18 @@
 </script>
 
 <template>
-  <div class="container-xxl">
-    <div class="row">
+  <div class="container-xxl py-5">
+    <div class="row gy-3 justify-content-center">
       <div class="col-md-8">
-        <router-link to="/">Home</router-link>
-        <router-link to="/projects">Projects</router-link>
-        <router-view></router-view>
+        <!-- navegation -->
+        <div class="border-bottom py-3 text-center">
+          <router-link to="/" class="text-500 mx-3 text-decoration-none py-1">Profile</router-link>
+          <router-link to="/projects" class="text-500 mx-3 text-decoration-none py-1">Projects</router-link>
+        </div>
+        <!--  content -->
+        <div class="content py-3">
+          <router-view></router-view>
+        </div>
       </div>
     </div>
   </div>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,7 +3,28 @@
 </script>
 
 <template>
-    <div>
-         <h1>HOme</h1>
+    <div class="position-relative">
+         <div class="row gy-3 gx-3 ">
+            <div class="col-md-3">
+                <div>
+                    <figure class="mb-0 rounded-circle overflow-hidden" style="max-width: 150px; max-height: 150px" >
+                         <img class="img-cover" src="https://www.wariexpeditions.com/storage/uploads/442-felix.jpg" alt="">
+                    </figure>
+                </div>
+            </div>
+            <div class="col-md-9">
+                <div class="position-relative">
+                    <h1 class="fw-bold fs-2">Felix Huari H. (Huari)</h1>
+                    <h2 class="fs-4">Programador Web,</h2>
+                     <p><i class="bi bi-geo-alt"></i> Matrix y el mundo real</p>
+                     <div class="d-flex align-items-center">
+                          <a href="" class="btn  bg-600 btn-sm text-white rounded-5 fw-bold"> <i class="bi bi-envelope"></i> felixhuari123@gmail.com</a>
+                          <a href="" class="ms-2 d-inline-block fs-5  btn-social"><i class="bi bi-file-earmark-richtext"></i></a>
+                          <a href="" class="ms-2 d-inline-block fs-5   btn-social"><i class="bi bi-github"></i></a>
+                          <a href="" class="ms-2 d-inline-block fs-5  btn-social"><i class="bi bi-linkedin"></i></a>
+                     </div>
+                </div>
+            </div>
+         </div>
     </div>
 </template>
