078c7824ba435b884d9ce86a8e692f8759f064b7
felix huari <felixhuari@gmail.com>
2024-08-15 15:45:45 -0500

    cambio 3

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,14 @@
 <script setup>
-
 </script>
 
 <template>
     <div class="position-relative">
-         <div class="row gy-3 gx-3 ">
+        <div class="row gy-3 gx-3">
             <div class="col-md-3">
                 <div>
-                    <figure class="mb-0 rounded-circle overflow-hidden" style="max-width: 150px; max-height: 150px" >
-                         <img class="img-cover" src="https://www.wariexpeditions.com/storage/uploads/442-felix.jpg" alt="">
+                    <figure class="mb-0 rounded-circle overflow-hidden" style="max-width: 150px; max-height: 150px">
+                        <img class="img-cover" src="https://www.wariexpeditions.com/storage/uploads/442-felix.jpg"
+                            alt="" />
                     </figure>
                 </div>
             </div>
@@ -16,15 +16,185 @@
                 <div class="position-relative">
                     <h1 class="fw-bold fs-2">Felix Huari H. (Huari)</h1>
                     <h2 class="fs-4">Programador Web,</h2>
-                     <p><i class="bi bi-geo-alt"></i> Matrix y el mundo real</p>
-                     <div class="d-flex align-items-center">
-                          <a href="" class="btn  bg-600 btn-sm text-white rounded-5 fw-bold"> <i class="bi bi-envelope"></i> felixhuari123@gmail.com</a>
-                          <a href="" class="ms-2 d-inline-block fs-5  btn-social"><i class="bi bi-file-earmark-richtext"></i></a>
-                          <a href="" class="ms-2 d-inline-block fs-5   btn-social"><i class="bi bi-github"></i></a>
-                          <a href="" class="ms-2 d-inline-block fs-5  btn-social"><i class="bi bi-linkedin"></i></a>
-                     </div>
+                    <p><i class="bi bi-geo-alt"></i> Matrix y el mundo real</p>
+                    <div class="d-flex align-items-center">
+                        <a href="" class="btn bg-600 btn-sm text-white rounded-5 fw-bold">
+                            <i class="bi bi-envelope"></i> felixhuari123@gmail.com</a>
+                        <a href="" class="ms-2 d-inline-block fs-5 btn-social"><i
+                                class="bi bi-file-earmark-richtext"></i></a>
+                        <a href="" class="ms-2 d-inline-block fs-5 btn-social"><i class="bi bi-github"></i></a>
+                        <a href="" class="ms-2 d-inline-block fs-5 btn-social"><i class="bi bi-linkedin"></i></a>
+                    </div>
                 </div>
             </div>
-         </div>
+        </div>
+        <!-- about my -->
+        <div class="position-relative py-3 mt-3">
+            <h2 class="text-white">Sobre mí</h2>
+            <p>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolorum,
+                molestiae ipsa. Molestias accusantium voluptas, officiis quidem quam ab
+                tempora vitae quas in. Et tempore nobis impedit eligendi blanditiis
+                assumenda culpa dolor deleniti. Odit delectus commodi accusantium iste
+                sapiente atque amet ratione, nobis quasi facere, officia, mollitia
+                laboriosam aliquam quia animi.
+            </p>
+        </div>
+        <hr />
+        <!-- tecnologias -->
+        <div class="position-relative">
+            <h3 class="text-white">Tecnologías</h3>
+            <div class="py-2">
+                <div class="d-inline-block px-3 mb-2 bg-950 ms-3 py-1 rounded-pill text-500 fw-600 ">
+                    <i class="bi bi-filetype-php"></i> <span>PHP</span>
+                </div>
+                <div class="d-inline-block  mb-2 px-3 bg-950 ms-3 py-1 rounded-pill text-500 fw-600">
+                    <i class="bi bi-filetype-jsx"></i> <span>Laravel</span>
+                </div>
+                <div class="d-inline-block px-3  mb-2 bg-950 ms-3 py-1 rounded-pill text-500 fw-600">
+                    <i class="bi bi-filetype-jsx"></i> <span>JavaScript</span>
+                </div>
+                <div class="d-inline-block px-3  mb-2 bg-950 ms-3 py-1 rounded-pill text-500 fw-600">
+                    <i class="bi bi-filetype-jsx"></i> <span>Vue</span>
+                </div>
+                <div class="d-inline-block px-3  mb-2 bg-950 ms-3 py-1 rounded-pill text-500 fw-600">
+                    <i class="bi bi-filetype-jsx"></i> <span>Bootstrap</span>
+                </div>
+                <div class="d-inline-block px-3  mb-2 bg-950 ms-3 py-1 rounded-pill text-500 fw-600">
+                    <i class="bi bi-filetype-jsx"></i> <span>SCSS</span>
+                </div>
+            </div>
+        </div>
+        <div class="position-relative mt-3">
+            <h3 class="text-white fs-4">Experiencia</h3>
+            <!-- col-1 -->
+            <div class="card mt-3 px-0 border-0">
+                <div class="card-body px-0">
+                    <div class="d-flex justify-content-between">
+                        <div class="">
+                            <div class="d-flex">
+                                <div class="">
+                                    <div class="icons bg-950 text-center rounded-circle">
+                                        <i class="bi bi-code text-primary fs-3"></i>
+                                    </div>
+                                </div>
+                                <div class="ms-3">
+                                    <h3 class="fs-5 fw-bold">SAM Travel Peru</h3>
+                                    <p class="fst-italic  text-white">Programador Web</p>
+                                    <p class="fs-sm-9">Lorem ipsum dolor sit amet consectetur adipisicing elit. At facilis atque delectus harum vel ex earum omnis deserunt quisquam, cupiditate sed eaque voluptatibus sapiente veniam, nam consequatur voluptates, consequuntur nobis pariatur in. Vitae quos repellendus deserunt aut corrupti facilis consequuntur doloremque quam. Praesentium quidem velit provident non modi libero fugiat!</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div>
+                            <span class="px-2 py-1 text-nowrap fs-sm-9 bg-950 text-600 rounded-pill fw-bold">2021 - Actualidad</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- col-2 -->
+            <div class="card mt-3 px-0 border-0">
+                <div class="card-body px-0">
+                    <div class="d-flex justify-content-between">
+                        <div class="">
+                            <div class="d-flex">
+                                <div class="">
+                                    <div class="icons bg-950 text-center rounded-circle">
+                                        <i class="bi bi-code text-primary fs-3"></i>
+                                    </div>
+                                </div>
+                                <div class="ms-3">
+                                    <h3 class="fs-5 fw-bold">Cusco Informatico </h3>
+                                    <p class="fst-italic text-white">Programador </p>
+                                    <p class="fs-sm-9">Lorem ipsum dolor sit amet consectetur adipisicing elit. At facilis atque delectus harum vel ex earum omnis deserunt quisquam, cupiditate sed eaque voluptatibus sapiente veniam, nam consequatur voluptates, consequuntur nobis pariatur in. Vitae quos repellendus deserunt aut corrupti facilis consequuntur doloremque quam. Praesentium quidem velit provident non modi libero fugiat!</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div>
+                            <span class="px-2 py-1 text-nowrap fs-sm-9 bg-950 text-600 rounded-pill fw-bold">2019 - 2021</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- proyectos -->
+        <div class="position-relative mt-3">
+            <h3 class="text-white fs-4">Proyectos</h3>
+            <!-- col-1 -->
+            <div class="card mt-3 px-0 border-0">
+                <div class="card-body px-0">
+                    <div class="d-flex justify-content-between">
+                        <div class="">
+                            <div class="d-flex">
+                                <div class="">
+                                    <div class="icons bg-950 text-center rounded-circle">
+                                        <i class="bi bi-code text-primary fs-3"></i>
+                                    </div>
+                                </div>
+                                <div class="ms-3">
+                                    <h3 class="fs-5 fw-bold">Near Delivery</h3>
+                                    <p class="text-white">Desarrollo de Ecommerce con Laravel</p>
+                                    <p class="fs-sm-9">Lorem ipsum dolor sit amet consectetur adipisicing elit. At facilis atque delectus harum vel ex earum omnis deserunt quisquam, cupiditate sed eaque voluptatibus sapiente veniam, nam consequatur voluptates, consequuntur nobis pariatur in. Vitae quos repellendus deserunt aut corrupti facilis consequuntur doloremque quam. Praesentium quidem velit provident non modi libero fugiat!</p>
+                                    <div>
+                                        <router-link to="/projects" ><i class="bi bi-link-45deg"></i></router-link>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- formacion -->
+        <div class="position-relative mt-3">
+            <h3 class="text-white fs-4">Formación</h3>
+            <!-- col-1 -->
+            <div class="card mt-3 px-0 border-0">
+                <div class="card-body px-0">
+                    <div class="d-flex justify-content-between">
+                        <div class="">
+                            <div class="d-flex">
+                                <div class="">
+                                    <div class="icons bg-950 text-center rounded-circle">
+                                        <i class="bi bi-code text-primary fs-3"></i>
+                                    </div>
+                                </div>
+                                <div class="ms-3">
+                                    <h3 class="fs-5 fw-bold">SAM Travel Peru</h3>
+                                    <p class="fst-italic  text-white">Programador Web</p>
+                                    <p class="fs-sm-9">Lorem ipsum dolor sit amet consectetur adipisicing elit. At facilis atque delectus harum vel ex earum omnis deserunt quisquam, cupiditate sed eaque voluptatibus sapiente veniam, nam consequatur voluptates, consequuntur nobis pariatur in. Vitae quos repellendus deserunt aut corrupti facilis consequuntur doloremque quam. Praesentium quidem velit provident non modi libero fugiat!</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div>
+                            <span class="px-2 py-1 text-nowrap fs-sm-9 bg-950 text-600 rounded-pill fw-bold">2021 - Actualidad</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- col-2 -->
+            <div class="card mt-3 px-0 border-0">
+                <div class="card-body px-0">
+                    <div class="d-flex justify-content-between">
+                        <div class="">
+                            <div class="d-flex">
+                                <div class="">
+                                    <div class="icons bg-950 text-center rounded-circle">
+                                        <i class="bi bi-code text-primary fs-3"></i>
+                                    </div>
+                                </div>
+                                <div class="ms-3">
+                                    <h3 class="fs-5 fw-bold">Cusco Informatico </h3>
+                                    <p class="fst-italic text-white">Programador </p>
+                                    <p class="fs-sm-9">Lorem ipsum dolor sit amet consectetur adipisicing elit. At facilis atque delectus harum vel ex earum omnis deserunt quisquam, cupiditate sed eaque voluptatibus sapiente veniam, nam consequatur voluptates, consequuntur nobis pariatur in. Vitae quos repellendus deserunt aut corrupti facilis consequuntur doloremque quam. Praesentium quidem velit provident non modi libero fugiat!</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div>
+                            <span class="px-2 py-1 text-nowrap fs-sm-9 bg-950 text-600 rounded-pill fw-bold">2019 - 2021</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
