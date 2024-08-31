be9c7fb8638858f014a7b5a857d2ef19df502c40
felix huari <felixhuari@gmail.com>
2024-08-31 12:02:01 -0500

    version actualizado el perfil

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -14,16 +14,16 @@
             </div>
             <div class="col-md-9">
                 <div class="position-relative">
-                    <h1 class="fw-bold fs-2">Felix Huari H. (Huari)</h1>
-                    <h2 class="fs-4">Programador Web,</h2>
-                    <p><i class="bi bi-geo-alt"></i> Matrix y el mundo real</p>
+                    <h1 class="fw-bold fs-2">Felipe Huari H. (fhuari)</h1>
+                    <h2 class="fs-4">Desarrollador Web Full Stack</h2>
+                    <p class="fs-sm-9"><i class="bi bi-geo-alt"></i> Impulsando tu visión en la web </p>
                     <div class="d-flex align-items-center">
-                        <a href="" class="btn bg-600 btn-sm text-white rounded-5 fw-bold">
+                        <a href="#" class="btn bg-600 btn-sm text-white rounded-5 fw-bold">
                             <i class="bi bi-envelope"></i> felixhuari123@gmail.com</a>
-                        <a href="" class="ms-2 d-inline-block fs-5 btn-social"><i
+                        <a href="#" class="ms-2 d-inline-block fs-5 btn-social"><i
                                 class="bi bi-file-earmark-richtext"></i></a>
-                        <a href="" class="ms-2 d-inline-block fs-5 btn-social"><i class="bi bi-github"></i></a>
-                        <a href="" class="ms-2 d-inline-block fs-5 btn-social"><i class="bi bi-linkedin"></i></a>
+                        <a href="#" class="ms-2 d-inline-block fs-5 btn-social"><i class="bi bi-github"></i></a>
+                        <a href="#" class="ms-2 d-inline-block fs-5 btn-social"><i class="bi bi-linkedin"></i></a>
                     </div>
                 </div>
             </div>
@@ -32,12 +32,12 @@
         <div class="position-relative py-3 mt-3">
             <h2 class="text-white">Sobre mí</h2>
             <p>
-                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolorum,
-                molestiae ipsa. Molestias accusantium voluptas, officiis quidem quam ab
-                tempora vitae quas in. Et tempore nobis impedit eligendi blanditiis
-                assumenda culpa dolor deleniti. Odit delectus commodi accusantium iste
-                sapiente atque amet ratione, nobis quasi facere, officia, mollitia
-                laboriosam aliquam quia animi.
+                Soy Felipe Huari, un desarrollador web full stack con más de 4 años de experiencia en la creación de
+                páginas web y aplicaciones web. Mi pasión por la tecnología me impulsa a estar en constante aprendizaje,
+                siempre buscando las mejores soluciones para cada proyecto. Soy una persona responsable, comprometida
+                con la calidad y la eficiencia, y disfruto transformando ideas en productos digitales que hacen la
+                diferencia. Mi objetivo es utilizar mis habilidades para ayudar a otros a alcanzar sus metas a través de
+                la tecnología.
             </p>
         </div>
         <hr />
@@ -63,6 +63,20 @@
                 <div class="d-inline-block px-3  mb-2 bg-950 ms-3 py-1 rounded-pill text-500 fw-600">
                     <i class="bi bi-filetype-jsx"></i> <span>SCSS</span>
                 </div>
+                <div class="d-inline-block px-3  mb-2 bg-950 ms-3 py-1 rounded-pill text-500 fw-600">
+                    <i class="bi bi-filetype-jsx"></i> <span>Wordpress <span class="fs-sm">(Creando plantilla desde cero)</span></span>
+                </div>
+            </div>
+            <div class="ps-5">
+                <b>Otros</b>
+                <div class="py-2">
+                    <div class="d-inline-block px-3 mb-2 bg-950 ms-3 py-1 rounded-pill text-500 fw-600 ">
+                        <i class="bi bi-filetype-php"></i> <span>SEO</span>
+                    </div>
+                    <div class="d-inline-block px-3 mb-2 bg-950 ms-3 py-1 rounded-pill text-500 fw-600 ">
+                        <i class="bi bi-filetype-php"></i> <span>SEM (google Ads)</span>
+                    </div>
+                </div>
             </div>
         </div>
         <div class="position-relative mt-3">
@@ -81,40 +95,19 @@
                                 <div class="ms-3">
                                     <h3 class="fs-5 fw-bold">SAM Travel Peru</h3>
                                     <p class="fst-italic  text-white">Programador Web</p>
-                                    <p class="fs-sm-9">Lorem ipsum dolor sit amet consectetur adipisicing elit. At facilis atque delectus harum vel ex earum omnis deserunt quisquam, cupiditate sed eaque voluptatibus sapiente veniam, nam consequatur voluptates, consequuntur nobis pariatur in. Vitae quos repellendus deserunt aut corrupti facilis consequuntur doloremque quam. Praesentium quidem velit provident non modi libero fugiat!</p>
+                                    <p class="fs-sm-9">En la actualidad, soy Programador Web, Web Master y responsable del área de sistemas en SAM Travel Peru, una agencia de viajes destacada. Mi labor incluye el desarrollo y mantenimiento de soluciones web, la optimización de plataformas digitales y la gestión de sistemas internos para garantizar un funcionamiento eficiente. En mi rol, aplico mis habilidades en, Wordpress, PHP, Laravel, Vue.js, y otras tecnologías para crear soluciones digitales que mejoran la experiencia de los usuarios .</p>
                                 </div>
                             </div>
                         </div>
                         <div>
-                            <span class="px-2 py-1 text-nowrap fs-sm-9 bg-950 text-600 rounded-pill fw-bold">2021 - Actualidad</span>
+                            <span class="px-2 py-1 text-nowrap fs-sm-9 bg-950 text-600 rounded-pill fw-bold">2021 -
+                                Actualidad</span>
                         </div>
                     </div>
                 </div>
             </div>
             <!-- col-2 -->
-            <div class="card mt-3 px-0 border-0">
-                <div class="card-body px-0">
-                    <div class="d-flex justify-content-between">
-                        <div class="">
-                            <div class="d-flex">
-                                <div class="">
-                                    <div class="icons bg-950 text-center rounded-circle">
-                                        <i class="bi bi-code text-primary fs-3"></i>
-                                    </div>
-                                </div>
-                                <div class="ms-3">
-                                    <h3 class="fs-5 fw-bold">Cusco Informatico </h3>
-                                    <p class="fst-italic text-white">Programador </p>
-                                    <p class="fs-sm-9">Lorem ipsum dolor sit amet consectetur adipisicing elit. At facilis atque delectus harum vel ex earum omnis deserunt quisquam, cupiditate sed eaque voluptatibus sapiente veniam, nam consequatur voluptates, consequuntur nobis pariatur in. Vitae quos repellendus deserunt aut corrupti facilis consequuntur doloremque quam. Praesentium quidem velit provident non modi libero fugiat!</p>
-                                </div>
-                            </div>
-                        </div>
-                        <div>
-                            <span class="px-2 py-1 text-nowrap fs-sm-9 bg-950 text-600 rounded-pill fw-bold">2019 - 2021</span>
-                        </div>
-                    </div>
-                </div>
-            </div>
+          
         </div>
         <!-- proyectos -->
         <div class="position-relative mt-3">
@@ -133,9 +126,14 @@
                                 <div class="ms-3">
                                     <h3 class="fs-5 fw-bold">Near Delivery</h3>
                                     <p class="text-white">Desarrollo de Ecommerce con Laravel</p>
-                                    <p class="fs-sm-9">Lorem ipsum dolor sit amet consectetur adipisicing elit. At facilis atque delectus harum vel ex earum omnis deserunt quisquam, cupiditate sed eaque voluptatibus sapiente veniam, nam consequatur voluptates, consequuntur nobis pariatur in. Vitae quos repellendus deserunt aut corrupti facilis consequuntur doloremque quam. Praesentium quidem velit provident non modi libero fugiat!</p>
+                                    <p class="fs-sm-9">Lorem ipsum dolor sit amet consectetur adipisicing elit. At
+                                        facilis atque delectus harum vel ex earum omnis deserunt quisquam, cupiditate
+                                        sed eaque voluptatibus sapiente veniam, nam consequatur voluptates, consequuntur
+                                        nobis pariatur in. Vitae quos repellendus deserunt aut corrupti facilis
+                                        consequuntur doloremque quam. Praesentium quidem velit provident non modi libero
+                                        fugiat!</p>
                                     <div>
-                                        <router-link to="/projects" ><i class="bi bi-link-45deg"></i></router-link>
+                                        <router-link to="/projects"><i class="bi bi-link-45deg"></i></router-link>
                                     </div>
                                 </div>
                             </div>
@@ -161,12 +159,18 @@
                                 <div class="ms-3">
                                     <h3 class="fs-5 fw-bold">SAM Travel Peru</h3>
                                     <p class="fst-italic  text-white">Programador Web</p>
-                                    <p class="fs-sm-9">Lorem ipsum dolor sit amet consectetur adipisicing elit. At facilis atque delectus harum vel ex earum omnis deserunt quisquam, cupiditate sed eaque voluptatibus sapiente veniam, nam consequatur voluptates, consequuntur nobis pariatur in. Vitae quos repellendus deserunt aut corrupti facilis consequuntur doloremque quam. Praesentium quidem velit provident non modi libero fugiat!</p>
+                                    <p class="fs-sm-9">Lorem ipsum dolor sit amet consectetur adipisicing elit. At
+                                        facilis atque delectus harum vel ex earum omnis deserunt quisquam, cupiditate
+                                        sed eaque voluptatibus sapiente veniam, nam consequatur voluptates, consequuntur
+                                        nobis pariatur in. Vitae quos repellendus deserunt aut corrupti facilis
+                                        consequuntur doloremque quam. Praesentium quidem velit provident non modi libero
+                                        fugiat!</p>
                                 </div>
                             </div>
                         </div>
                         <div>
-                            <span class="px-2 py-1 text-nowrap fs-sm-9 bg-950 text-600 rounded-pill fw-bold">2021 - Actualidad</span>
+                            <span class="px-2 py-1 text-nowrap fs-sm-9 bg-950 text-600 rounded-pill fw-bold">2021 -
+                                Actualidad</span>
                         </div>
                     </div>
                 </div>
@@ -185,12 +189,18 @@
                                 <div class="ms-3">
                                     <h3 class="fs-5 fw-bold">Cusco Informatico </h3>
                                     <p class="fst-italic text-white">Programador </p>
-                                    <p class="fs-sm-9">Lorem ipsum dolor sit amet consectetur adipisicing elit. At facilis atque delectus harum vel ex earum omnis deserunt quisquam, cupiditate sed eaque voluptatibus sapiente veniam, nam consequatur voluptates, consequuntur nobis pariatur in. Vitae quos repellendus deserunt aut corrupti facilis consequuntur doloremque quam. Praesentium quidem velit provident non modi libero fugiat!</p>
+                                    <p class="fs-sm-9">Lorem ipsum dolor sit amet consectetur adipisicing elit. At
+                                        facilis atque delectus harum vel ex earum omnis deserunt quisquam, cupiditate
+                                        sed eaque voluptatibus sapiente veniam, nam consequatur voluptates, consequuntur
+                                        nobis pariatur in. Vitae quos repellendus deserunt aut corrupti facilis
+                                        consequuntur doloremque quam. Praesentium quidem velit provident non modi libero
+                                        fugiat!</p>
                                 </div>
                             </div>
                         </div>
                         <div>
-                            <span class="px-2 py-1 text-nowrap fs-sm-9 bg-950 text-600 rounded-pill fw-bold">2019 - 2021</span>
+                            <span class="px-2 py-1 text-nowrap fs-sm-9 bg-950 text-600 rounded-pill fw-bold">2019 -
+                                2021</span>
                         </div>
                     </div>
                 </div>
