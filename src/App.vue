d07054f56e7dd0e96123888460971bd8280a51d1
felix huari <felixhuari@gmail.com>
2024-08-31 10:48:09 -0500

    changed  col dimanic

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,19 @@
 <script setup>
+import { useRoute } from 'vue-router';
 
-
+const route = useRoute();
 </script>
 
 <template>
   <div class="container-xxl py-5">
     <div class="row gy-3 justify-content-center">
-      <div class="col-md-8">
-        <!-- navegation -->
+      <div :class="route.path === '/projects' ? 'col-md-12' : 'col-md-8'">
+        <!-- navegación -->
         <div class="border-bottom py-3 text-center">
           <router-link to="/" class="text-500 mx-3 text-decoration-none py-1">Profile</router-link>
           <router-link to="/projects" class="text-500 mx-3 text-decoration-none py-1">Projects</router-link>
         </div>
-        <!--  content -->
+        <!-- contenido -->
         <div class="content py-3">
           <router-view></router-view>
         </div>
--- src/views/Projects.vue
+++ src/views/Projects.vue
@@ -1,9 +1,15 @@
 <script setup>
-
+import samfile from '../assets/image/samtravel.png'
 </script>
 
 <template>
-    <div>
-         <h1>Projects</h1>
+    <div class="row">
+        <div class="col-md-6">
+             <div class="card px-0">
+                  <figure>
+                    <img class="img-fluid" :src="samfile" alt="">
+                  </figure>
+             </div>
+        </div>
     </div>
 </template>
